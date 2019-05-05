dd116b3bdc878cc56cdf766f03cc458914c02808
scott967 <75270.3703@earthlink.net>
2019-05-05 13:06:57 -1000

    Debug the script

--- script.music.userrating/default.py
+++ script.music.userrating/default.py
@@ -21,21 +21,30 @@ class Rating():
         self.main()
         
     def main(self):
-        xbmcgui.Window(10000).setProperty(__addon_id__ + '_running')
+        xbmcgui.Window(10000).setProperty(__addon_id__ + '_running',  'true')
         # detect that user entred a valid rating (integer 0 - 10)
-        if len(sys.argv) != 3 or  int(sys.argv[2]) < 0 or int(sys.argv[2]) > 10:
-             xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(38022) + ', 2000, ' + xbmcgui.NOTIFICATION_INFO + ')')
+        if len(sys.argv) != 2 or  int(sys.argv[1]) < 0 or int(sys.argv[1]) > 10:
+            # No rating
+            xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(38022) + ', 2000, ' + xbmcgui.NOTIFICATION_INFO + ')')
             return
-        newRating = int(sys.argv[2])
+        newRating = int(sys.argv[1])
         # detect no song playing
         if not xbmc.getInfoLabel('MusicPlayer.DBID'):
+            # Couldn't get songs from database
             xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(16034) + ', 2000, ' + xbmcgui.NOTIFICATION_INFO + ')')
             return
         
-        jsonNew = '{"jsonrpc": "2.0", "id": 1, "method": "AudioLibrary.SetSongDetails' + 'Library.id" : ' + xbmc.getInfoLabel('MusicPlayer.DBID') + ', "userrating": ' + sys.argv[2]+ '}'
+        jsonNew = '{"jsonrpc": "2.0", "id": 1, "method": "AudioLibrary.SetSongDetails", "params": { "songid" : ' + xbmc.getInfoLabel('MusicPlayer.DBID') + ', "userrating": ' + sys.argv[1] + ' }}'
+        # xbmc.log('music.userrating JOSN call:  ' + jsonNew)
         jsonResponse = xbmc.executeJSONRPC(jsonNew)
-        if jsonResponse:
-            xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(38018) + ' : ' + sys.argv[2] + ', 3000, ' + xbmcgui.NOTIFICATION_INFO + ')')
+        # xbmc.log('music.userrating response:  ' + jsonResponse)
+        if jsonResponse and ('OK' in jsonResponse):
+            # My rating
+            xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(38018) + ' : ' + sys.argv[1] + ', 3000, ' + xbmcgui.NOTIFICATION_INFO + ')')
+        else:
+            # Update failed
+            xbmc.executebuiltin('Notification(' + __addonname__ + ', ' + xbmc.getLocalizedString(113) + ', 3000, ' + xbmcgui.NOTIFICATION_INFO + ')')
+            
 
  # lock script to prevent duplicates
 if (xbmcgui.Window(10000).getProperty(__addon_id__ + '_running') != 'True'):
